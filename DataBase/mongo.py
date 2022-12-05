f56b21f2eedbf86919fee7a93be1a8f72d2fcf33
Itay Segev <itaysegev11@gmail.com>
2022-12-05 20:31:24 +0200

    build the data set from json file

--- DataBase/mongo.py
+++ DataBase/mongo.py
@@ -5,24 +5,26 @@ import json
 import os
 
 
-def mongoimport(csv_path, db_name, collection_name, client):
-    """ Imports a csv file at path csv_name to a mongo colection
-    returns: count of the documants in the new collection
-    """
-    db = client[db_name]
-    coll = db[collection_name]
-    data = pd.read_csv(csv_path)
-    payload = json.loads(data.to_json(orient='records'))
-    coll.remove()
-    coll.insert(payload)
-    return coll.count()
+def mongoimport_json(json_path, db_name, collection_name, client):
+    """ Imports a json file at path json_path to a mongo colection
+       returns: count of the documants in the new collection
+       """
+    with open(json_path, encoding='utf8') as f:
+        db = client[db_name]
+        coll = db[collection_name]
+        payload = json.load(f)
+        coll.drop()
+        coll.insert_many(payload)
+    return coll.estimated_document_count()
 
 
-csv_path = os.path.abspath('apt2.csv')
+json_path = os.path.abspath('json')
 db_name = 'test'
 collection_name = 'apt2'
 cluster = f"mongodb+srv://itaysegev11:{pswd}@cluster0.07hxrbu.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(cluster)
-mongoimport(csv_path, db_name, collection_name, client)
+
+
+print(mongoimport_json(json_path, db_name, collection_name, client))
 
 print(client.list_database_names())
